fcdaa3d067d95efeaad06fb26df20526364f4f58
Francesco Iorio <iorio@ebi.ac.uk>
2015-09-30 17:28:55 +0100

    changing global setting system (incremental)

--- R/Pipelines/FI.GDSC.ANOVA.Preamble.R
+++ R/Pipelines/FI.GDSC.ANOVA.Preamble.R
@@ -1,72 +1,35 @@
-## ANOVA paths and filenames
-
-annotations.master_list.fn<-'../../Data/Cell_lines/Annotations/MASTER_LIST_20150115.rdata'
-#annotations.cosmic2sampleMap.fn<-'../../Data/Cell_lines/Annotations/DanielCosmic2sampleNames.rdata'
-
-gdscANOVA.screening.fn<-'../../Data/DrugScreening/September2015/simplyIC50s-Final-RMSE-Filtered-0.3.rdata'
-gdscANOVA.screening.DRcurves.fn<-'../../Data/DrugScreening/DoseResponseCurves.rdata'
-gdscANOVA.cellLineFeatures.dir<-'../../Data/Cell_lines/MultiOmicBEMs/'
-gdscANOVA.additionalFeatures.fn<-'../../Data/DR.Lancet/Cluster_BEMs/20150925//COREAD.rdata'
-
-
-gdscANOVA.results.dir<-'../../Results/DR.Lancet/ANOVA/'
-
-
-gdscANOVA.maxTestedConc.fn<-'../../Data/DrugScreening/maximalTestedConcentrations.rdata'
-gdscANOVA.drugProperties.fn<-'../../Data/DrugScreening/v17_DRUG_PROPS_01072014.rdata'
-gdscANOVA.drugOwnership.fn<-'../../Data/DrugScreening/DRUG_BY_COMPANIES_20150126.rdata'
-
-
-## ANOVA file name suffixes
-gdscANOVA.cellLineFeatures.fn_suffix<-'_simple_MOBEM.rdata'
-
-## ANOVA settings
-gdscANOVA.settings.SCREENING_VERSION<-paste('v17 - up to',Sys.Date())
-gdscANOVA.settings.DRUG_domain<-"GDSC1000_paper_set"
-#gdscANOVA.settings.CELL_LINES<-'COREAD'
-#gdscANOVA.settings.includeMSI_Factor<-TRUE
-gdscANOVA.settings.analysisType<-'CS'
-#gdscANOVA.settings.additionalFeatures<-TRUE
-gdsdANOVA.settings.additionalFeaturesOnly<-TRUE
-gdscANOVA.settings.excludeHyperMetData<-TRUE
-gescANOVA.settings.oneGeneOnly<-NULL
-gdscANOVA.settings.resPackageHeader<-paste('DR.Lancet Project ANOVA',gdscANOVA.settings.CELL_LINES,'specific result package')
-gdscANOVA.settings.resPackageIncludeSangerLogo<-FALSE
-gdscANOVA.settings.resPackageIncludeNKILogo<-FALSE
-gdscANOVA.settings.resPackageIncludeEBIlogo<-TRUE
-gdscANOVA.settings.featFactorPopulationTh<-3
-gdscANOVA.settings.MSIfactorPopulationTh<-2
-
-gdscANOVA.settings.pval_correction_method<-'fdr'
-
-if (gdscANOVA.settings.CELL_LINES == 'PANCAN'){
-  gdscANOVA.settings.pval_TH<-Inf  
-}else{
-  gdscANOVA.settings.pval_TH<-Inf
+gdscANOVA_Preamble<-function(ANOVA_setting_file='HTML_templates_and_elements/FI.GDSC.ANOVA.Settings_file_template.csv'){
+  
+  settings<-read.csv(ANOVA_setting_file,stringsAsFactors=FALSE)
+  settings<-settings[setdiff(1:nrow(settings),c(1:3,11,13))]
+  
+  GLOBAL_SETTINGS<-settings$Analysis.Description
+  
+  
+  load(gdscANOVA.screening.fn)
+  load(paste(gdscANOVA.cellLineFeatures.dir,gdscANOVA.settings.CELL_LINES,gdscANOVA.cellLineFeatures.fn_suffix,sep=''))
+  
+  TOTALBEM <-MoBEM
+  
+  load(gdscANOVA.drugProperties.fn)
+  load(gdscANOVA.drugOwnership.fn)
+  load(gdscANOVA.maxTestedConc.fn)
+  load(gdscANOVA.screening.DRcurves.fn)
+  
+  load(annotations.master_list.fn)
+  load(annotations.cosmic2sampleMap.fn)
+  
+  if(gdscANOVA.settings.additionalFeatures){
+    print('Loading Additional Features...')
+    load(gdscANOVA.additionalFeatures.fn)
+    gdscANOVA.additionalFeatures=BEM
+    print('+ Done!')
+  }else{
+    gdscANOVA.additionalFeatures=NULL
+  }
+  
+  
+  
+  
 }
-
-gdscANOVA.settings.FDR_TH<-25
-
-load(gdscANOVA.screening.fn)
-load(paste(gdscANOVA.cellLineFeatures.dir,gdscANOVA.settings.CELL_LINES,gdscANOVA.cellLineFeatures.fn_suffix,sep=''))
-
-TOTALBEM <-MoBEM
-
-load(gdscANOVA.drugProperties.fn)
-load(gdscANOVA.drugOwnership.fn)
-load(gdscANOVA.maxTestedConc.fn)
-load(gdscANOVA.screening.DRcurves.fn)
-
-load(annotations.master_list.fn)
-load(annotations.cosmic2sampleMap.fn)
-
-if(gdscANOVA.settings.additionalFeatures){
-  print('Loading Additional Features...')
-  load(gdscANOVA.additionalFeatures.fn)
-  gdscANOVA.additionalFeatures=BEM
-  print('+ Done!')
-}else{
-  gdscANOVA.additionalFeatures=NULL
-}
-
 
